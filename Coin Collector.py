7e2a75c3ecd809acd4adc5228cb3fce31c8d0e2e
Paul Jefferies <jefferie@umich.edu>
2019-01-09 21:55:27 -0700

    Initial commit. Pretty much done.

---
+++ Coin Collector.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 22:16:18 2019
+
+@author: PaulJ
+"""
+
+# import time
+from random import randint
+import pgzrun
+
+WIDTH = 400
+HEIGHT = 400
+score = 0
+game_over = False
+
+fox = Actor('fox')
+fox.pos = 100, 100
+
+coin = Actor('coin')
+coin.pos = 200, 200
+
+
+def draw():
+    screen.fill('green')
+    fox.draw()
+    coin.draw()
+    screen.draw.text('Score: ' + str(score), color='black', topleft=(10, 10))
+
+    if game_over:
+        screen.fill('pink')
+        screen.draw.text('Final Score: ' + str(score), topleft=(10, 10),
+                         fontsize=60)
+
+def place_coin():
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT - 20))
+
+def time_up():
+    global game_over
+    game_over = True
+
+def update():
+    global score
+
+    if keyboard.left:
+        fox.x = fox.x - 2
+    elif keyboard.right:
+        fox.x = fox.x + 2
+    elif keyboard.up:
+        fox.y = fox.y - 2
+    elif keyboard.down:
+        fox.y = fox.y + 2
+
+    coin_collected = fox.colliderect(coin)
+
+    if coin_collected:
+        score = score + 10
+        place_coin()
+
+clock.schedule(time_up, 7.0)
+place_coin()
+
+
+pgzrun.go()
